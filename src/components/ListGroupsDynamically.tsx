801f4ce2b25d6d9d5cb03de2eeab73690d9528d5
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2026-01-05 17:25:16 +0545

    Implemented conditonal renderings

--- src/components/ListGroupsDynamically.tsx
+++ src/components/ListGroupsDynamically.tsx
@@ -1,9 +1,13 @@
 function ListGroupsDynamically() {
-  const cities = ["New York", "Paris", "Dallas", "Boston", "San Franciso"];
-
+  let cities = ["New York", "Paris", "Dallas", "Boston", "San Franciso"];
+  //   const messageFunction = () => {
+  //     return cities.length === 0 ? <p>No items found</p> : null;
+  //   };
   return (
     <>
       <h1>List Groups</h1>
+      {/* {messageFunction()} */}
+      {cities.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {cities.map((city) => (
           <li key={city}>{city}</li>
